8b8f30141ccf03d71ac054d0a164e0548da55dd5
Anil kumar nayak <anilkumarnayak639@gmail.com>
2024-01-05 10:39:06 +0530

    Post and Home updated

---
+++ Client/src/utils/index.js
@@ -0,0 +1,12 @@
+import surpriseMePrompts from "../Constants/Constants";
+
+const getRandomPrompt = (prompt) => {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+};
+
+export default getRandomPrompt;
